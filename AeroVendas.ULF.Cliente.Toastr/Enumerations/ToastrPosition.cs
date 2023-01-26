cbbfe64ea71a8d9a16526754a75f6ab4b756670e
joaobiener <joaobiener@hotmail.com>
2023-01-26 17:50:18 -0300

    Atualizações

--- AeroVendas.ULF.Cliente.Toastr/Enumerations/ToastrPosition.cs
+++ AeroVendas.ULF.Cliente.Toastr/Enumerations/ToastrPosition.cs
@@ -6,6 +6,7 @@ namespace AeroVendas.ULF.Cliente.Toastr.Enumerations
 	{
 		[Description("toast-top-left")] TopLeft,
 		[Description("toast-top-right")] TopRight,
+		[Description("toast-top-center")] TopCenter,
 		[Description("toast-bottom-left")] BottomLeft,
 		[Description("toast-bottom-right")] BottomRight
 	}
--- AeroVendas.ULF.Cliente/Pages/Login.razor.cs
+++ AeroVendas.ULF.Cliente/Pages/Login.razor.cs
@@ -1,13 +1,16 @@
 ﻿using AeroVendas.ULF.Cliente.HttpRepository;
 using Shared.DataTransferObjects;
 using Microsoft.AspNetCore.Components;
+using AeroVendas.ULF.Cliente.Toastr.Enumerations;
+using AeroVendas.ULF.Cliente.Toastr.Services;
+using AeroVendas.ULF.Cliente.Toastr;
 
 namespace AeroVendas.ULF.Cliente.Pages
 {
     public partial class Login
 	{
 		private UserForAuthenticationDto _userForAuthentication = new UserForAuthenticationDto();
-
+		private bool loading;
 		[Inject]
 		public IAuthenticationService? AuthenticationService { get; set; }
 
@@ -16,16 +19,35 @@ namespace AeroVendas.ULF.Cliente.Pages
 
 		public bool ShowAuthError { get; set; }
 		public string? Error { get; set; }
+		[Inject]
+		public ToastrService? ToastrService { get; set; }
 
+		private async Task ShowToastrInfo(string message)
+		{
+		
+
+			var options = new ToastrOptions
+			{
+				CloseButton = true,
+				HideDuration = 300,
+				HideMethod = ToastrHideMethod.SlideUp,
+				ShowMethod = ToastrShowMethod.SlideDown,
+				Position = ToastrPosition.TopCenter
+			};
+			await ToastrService.ShowInfoMessage(message, options);
+		}
 		public async Task ExecuteLogin()
 		{
 			ShowAuthError = false;
-
+			loading = true;
 			var result = await AuthenticationService.Login(_userForAuthentication);
+			loading = false;
 			if (!result.IsAuthSuccessful)
 			{
+				
 				Error = result.ErrorMessage;
-				ShowAuthError = true;
+				ShowToastrInfo(Error);
+				//ShowAuthError = true;
 			}
 			else
 			{
