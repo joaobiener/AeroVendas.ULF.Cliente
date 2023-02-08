d4553b1bd230ada834a3460fdf68626d4053205e
joaobiener <joaobiener@hotmail.com>
2023-02-08 15:24:29 -0300

    Inclusão do partial

---
+++ AeroVendas.ULF.Cliente/Pages/EditorWidget.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace AeroVendas.ULF.Cliente.Pages;
+
+    public partial class EditorWidget
+    {
+        private string content = "";
+        bool disable = false;
+        private Dictionary<string, object> editorConf = new Dictionary<string, object>{
+        {"toolbar", "save | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        },
+        {"plugins",  "['advlist autolink lists link image charmap print preview anchor,save, " +
+        "'searchreplace visualblocks code fullscreen'," +
+        "'insertdatetime media table paste code help wordcount']"
+        },
+        {"content_style",  "body { font-family:Helvetica,Arial,sans-serif; font-size:14px" }
+        //{"skin", "oxide-dark" }
+        ////{"content_css", "dark" }
+
+
+        };
+    }
+
