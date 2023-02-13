8d5d40b8c378bb273562db9fd119bfca807df238
joaobiener <joaobiener@hotmail.com>
2023-02-13 09:21:21 -0300

    Pagina de visualização

---
+++ AeroVendas.ULF.Cliente/Pages/MensagemHTMLPreview.razor.cs
@@ -0,0 +1,22 @@
+﻿using AeroVendas.ULF.Cliente.HttpRepository;
+using Entities.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace AeroVendas.ULF.Cliente.Pages
+{
+    public partial class MensagemHTMLPreview
+	{
+		public MensagemHtml Mensagem { get; set; } = new MensagemHtml();
+
+		[Inject]
+		public IMensagemHtmlHttpRepository? MensagemRepo { get; set; }
+
+		[Parameter]
+		public Guid MensagemId { get; set; }
+
+		protected async override Task OnInitializedAsync()
+		{
+			Mensagem = await MensagemRepo.GetMensagemHTMLById(MensagemId);
+		}
+	}
+}
